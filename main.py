5a62c4f55d11a810812f87e88410ba6ce63e66fe
ghigoviu <ghigoviu@gmail.com>
2022-03-06 23:55:14 -0600

    Agregado recorrido en anchura

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import Arbol
 from Nodo import Nodo
+from Arbol import Arbol
 
 if __name__ == '__main__':
     print("Bienvenido")
@@ -13,12 +15,17 @@ if __name__ == '__main__':
     #Problema 1
     raiz = Nodo(1,'A')
     #Agregando nodos
+    copiaNodos = listaNodos[:]
     listaNodos[12].__add__(listaNodos[14]) #Nodo P a nodo N
-    listaNodos[13].__add__(listaNodos[14]) #Nodo P a nodo O
+    listaNodos[13].__add__(copiaNodos[14]) #Nodo P a nodo O
     listaNodos[11].__add__(listaNodos[12], listaNodos[13]) #Nodo N y O a nodo M
     listaNodos[8].__add__(listaNodos[10], listaNodos[11]) #Nodo L y M a nodo J
     listaNodos[6].__add__(listaNodos[8], listaNodos[9]) #Nodo J y K a nodo H
-    listaNodos[7].__add__(listaNodos[8], listaNodos[9]) #Nodo J y K a nodo I
+    #Ahora debemos copiar todos los datos del nodo J y K al nodo H
+    copiaNodoJ, copiaNodoK = listaNodos[8:10]
+    listaNodos[7].__add__(copiaNodoJ, copiaNodoK)
+
+    #listaNodos[7].__add__(listaNodos[8], listaNodos[9]) #Nodo J y K a nodo I
     listaNodos[4].__add__(listaNodos[6]) #Nodo H a nodo F
     listaNodos[5].__add__(listaNodos[7]) #Nodo I a nodo G
     listaNodos[3].__add__(listaNodos[4], listaNodos[5]) #Nodo F y G a nodo E
@@ -27,20 +34,23 @@ if __name__ == '__main__':
     problema2_raiz = Nodo()
     problema3_raiz = Nodo()
 
-print("Imprimiendo lista de nodos")
-for nodo in raiz.adyacentes:
-    print(nodo)
+    print("Imprimiendo lista de nodos")
+    for nodo in raiz.adyacentes:
+        print(nodo)
+
+    arbol1 = Arbol(raiz)
+    arbol1.recorridoAnchura(raiz)
 
 '''
 Algoritmo de profundidad, utiliza una pila
-def DFS(graph,start):
+def DFS(self, start):
     queue = []
     queue.append(start)
     seen = set()
     seen.add(start)
     while len(queue)>0:
         vert = queue.pop () # Esto muestra una pila, el último en entrar es el primero en salir
-        nodes = graph[vert]
+        nodes = vert.adyacencia
         for w in nodes:
             if w not in seen:
                 queue.append(w)
