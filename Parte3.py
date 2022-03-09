62bbd355f27436b79be07523cbd28305c9d467c6
ghigoviu <ghigoviu@gmail.com>
2022-03-08 22:39:59 -0600

    Programa 3 listo

--- Parte3.py
+++ Parte3.py
@@ -1,212 +1,118 @@
-class arbol:
-    def __init__(self,valor,nivel): 
-        self.valor = valor 
-        self.hijos = []
+class Node:
+    def __init__(self,datos,nivel,fvalores):
+        self.datos = datos
         self.nivel = nivel
+        self.fvalores = fvalores
 
-def iterada(raiz, limite, ladoBusqueda, valorB):
-    nodo = []
-    pila = [] 
-    pila.append(raiz)  
-    while(pila): 
-        nodo = pila.pop()
-        if(nodo.nivel<=limite): 
-            print("Ruta: \n", imprimir(nodo.valor))
-            print("Nivel: ",nodo.nivel)
-            if (nodo.valor==valorB): 
-                print("Tablero (nodo) encontrado: \n", imprimir(nodo.valor),"En el nivel: ",nodo.nivel)
-                flag=1
-                return True 
-                break  
-            else:
-                if(ladoBusqueda == 1):
-                    for value in reversed(nodo.hijos): 
-                        pila.append (value)
-                elif (ladoBusqueda == 2):
-                    for value in nodo.hijos: 
-                        pila.append (value)  
-    return False
-
-def imprimir(valor):
-    s = [[str(e) for e in row] for row in valor]
-    lens = [max(map(len, col)) for col in zip(*s)]
-    fmt = '\t'.join('{{:{}}}'.format(x) for x in lens)
-    table = [fmt.format(*row) for row in s]
-    return '\n'.join(table)
-
-raiz=arbol([[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0],[0, 0, 0, 0]],0)
-###limite 1
-#hijos de 1
-raiz.hijos.append(arbol([[1, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0,0, 0, 0]], 1))
-raiz.hijos.append(arbol([[0, 0, 0, 0], [1, 0, 0, 0], [0, 0, 0, 0], [0,0, 0, 0]], 1))
-raiz.hijos.append(arbol([[0, 0, 0, 0], [0, 0, 0, 0], [1, 0, 0, 0], [0,0, 0, 0]], 1))
-raiz.hijos.append(arbol([[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [1,0, 0, 0]], 1))
-##limite 2
-#hijos de 2
-raiz.hijos[0].hijos.append(arbol([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 0,0], [0, 0, 0, 0]], 2))
-raiz.hijos[0].hijos.append(arbol([[1, 0, 0, 0], [0, 0, 1, 0], [0, 0, 0,0], [0, 0, 0, 0]], 2))
-#hijos de 3
-raiz.hijos[1].hijos.append(arbol([[0, 1, 0, 0], [1, 0, 0, 0], [0, 0, 0,0], [0, 0, 0, 0]], 2))
-raiz.hijos[1].hijos.append(arbol([[0, 0, 1, 0], [1, 0, 0, 0], [0, 0, 0,0], [0, 0, 0, 0]], 2))
-#hijos de 4
-raiz.hijos[2].hijos.append(arbol([[0, 1, 0, 0], [0, 0, 0, 0], [1, 0, 0,0], [0, 0, 0, 0]], 2))
-raiz.hijos[2].hijos.append(arbol([[0, 0, 1, 0], [0, 0, 0, 0], [1, 0, 0,0], [0, 0, 0, 0]], 2))
-#hijos de 5
-raiz.hijos[3].hijos.append(arbol([[0, 1, 0, 0], [0, 0, 0, 0], [0, 0, 0,0], [1, 0, 0, 0]], 2))
-raiz.hijos[3].hijos.append(arbol([[0, 0, 1, 0], [0, 0, 0, 0], [0, 0, 0,0], [1, 0, 0, 0]], 2))
-##limite 3
-#hijos de 6
-raiz.hijos[0].hijos[0].hijos.append(arbol([[1, 0, 0, 0], [0, 1, 0, 0],[0, 0, 1, 0], [0, 0, 0, 0]], 3))
-raiz.hijos[0].hijos[0].hijos.append(arbol([[1, 0, 0, 0], [0, 1, 0, 0],[0, 0, 0, 1], [0, 0, 0, 0]], 3))
-#hijos de 7
-raiz.hijos[0].hijos[1].hijos.append(arbol([[1, 0, 0, 0], [0, 0, 1, 0],[0, 0, 0, 1], [0, 0, 0, 0]], 3))
-raiz.hijos[0].hijos[1].hijos.append(arbol([[1, 0, 0, 0], [0, 0, 1, 0],[0, 0, 0, 0], [0, 0, 0, 1]], 3))
-#hijos de 8
-raiz.hijos[1].hijos[0].hijos.append(arbol([[0, 1, 0, 0], [1, 0, 0, 0],[0, 1, 0, 0], [0, 0, 0, 0]], 3))
-raiz.hijos[1].hijos[0].hijos.append(arbol([[0, 1, 0, 0], [1, 0, 0, 0],[0, 0, 1, 0], [0, 0, 0, 0]], 3))
-#hijos de 9
-raiz.hijos[1].hijos[1].hijos.append(arbol([[0, 0, 1, 0], [1, 0, 0, 0],[0, 0, 1, 0], [0, 0, 0, 0]], 3))
-raiz.hijos[1].hijos[1].hijos.append(arbol([[0, 0, 1, 0], [1, 0, 0, 0],[0, 0, 0, 1], [0, 0, 0, 0]], 3))
-#hijos de 10
-raiz.hijos[2].hijos[0].hijos.append(arbol([[0, 1, 0, 0], [0, 0, 1, 0],[1, 0, 0, 0], [0, 0, 0, 0]], 3))
-raiz.hijos[2].hijos[0].hijos.append(arbol([[0, 1, 0, 0], [0, 0, 0, 1],[1, 0, 0, 0], [0, 0, 0, 0]], 3))
-#hijos de 11
-raiz.hijos[2].hijos[1].hijos.append(arbol([[0, 0, 1, 0], [0, 0, 1, 0],[1, 0, 0, 0], [0, 0, 0, 0]], 3))
-raiz.hijos[2].hijos[1].hijos.append(arbol([[0, 0, 1, 0], [0, 0, 0, 1],[1, 0, 0, 0], [0, 0, 0, 0]], 3))
-#hijos de 12
-raiz.hijos[3].hijos[0].hijos.append(arbol([[0, 1, 0, 0], [0, 0, 0, 0],[0, 0, 1, 0], [1, 0, 0, 0]], 3))
-raiz.hijos[3].hijos[0].hijos.append(arbol([[0, 1, 0, 0], [0, 0, 0, 0],[0, 0, 0, 1], [1, 0, 0, 0]], 3))
-#hijos de 13
-raiz.hijos[3].hijos[1].hijos.append(arbol([[0, 0, 1, 0], [0, 0, 0, 0],[1, 0, 0, 0], [1, 0, 0, 0]], 3))
-raiz.hijos[3].hijos[1].hijos.append(arbol([[0, 0, 1, 0], [0, 0, 0, 0],[0, 1, 0, 0], [1, 0, 0, 0]], 3))
-##limite 4
-#hijos de 14
-raiz.hijos[0].hijos[0].hijos[0].hijos.append(arbol([[1, 0, 0, 0], [0,1, 0, 0], [0, 0, 1, 0], [1, 0, 0, 0]], 4))
-#hijos de 15
-raiz.hijos[0].hijos[0].hijos[1].hijos.append(arbol([[1, 0, 0, 0], [0,1, 0, 0], [0, 0, 0, 1], [0, 1, 0, 0]], 4))
-#hijos de 16
-raiz.hijos[0].hijos[1].hijos[0].hijos.append(arbol([[1, 0, 0, 0], [0,0, 1, 0], [0, 0, 0, 1], [1, 0, 0, 0]], 4))
-#hijos de 17
-raiz.hijos[0].hijos[1].hijos[1].hijos.append(arbol([[1, 0, 0, 0], [0,0, 1, 0], [1, 0, 0, 0], [0, 0, 0, 1]], 4))
-#hijos de 18
-raiz.hijos[1].hijos[0].hijos[0].hijos.append(arbol([[0, 1, 0, 0], [1,0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0]], 4))
-#hijos de 19
-raiz.hijos[1].hijos[0].hijos[1].hijos.append(arbol([[0, 1, 0, 0], [1,0, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]], 4))
-#hijos de 20
-raiz.hijos[1].hijos[1].hijos[0].hijos.append(arbol([[0, 0, 1, 0], [1,0, 0, 0], [0, 0, 1, 0], [1, 0, 0, 0]], 4))
-#hijos de 21
-raiz.hijos[1].hijos[1].hijos[1].hijos.append(arbol([[0, 0, 1, 0], [1,0, 0, 0], [0, 0, 0, 1], [0, 1, 0, 0]], 4))
-#hijos de 22
-raiz.hijos[2].hijos[0].hijos[0].hijos.append(arbol([[0, 1, 0, 0], [0,0, 1, 0], [1, 0, 0, 0], [0, 1, 0, 0]], 4))
-#hijos de 23
-raiz.hijos[2].hijos[0].hijos[1].hijos.append(arbol([[0, 1, 0, 0], [0,0, 0, 1], [1, 0, 0, 0], [0, 0, 1, 0]], 4))#
-#hijos de 24
-raiz.hijos[2].hijos[1].hijos[0].hijos.append(arbol([[0, 0, 1, 0], [0,0, 1, 0], [1, 0, 0, 0], [0, 1, 0, 0]], 4))
-#hijos de 25
-raiz.hijos[2].hijos[1].hijos[1].hijos.append(arbol([[0, 0, 1, 0], [0,0, 0, 1], [1, 0, 0, 0],[0, 1, 0, 0]],4))
-#hijos de 26
-raiz.hijos[3].hijos[0].hijos[0].hijos.append(arbol([[0, 1, 0, 0], [0,0, 1, 0], [0, 0, 1, 0],[1, 0, 0, 0]],4))
-#hijos de 27
-raiz.hijos[3].hijos[0].hijos[1].hijos.append(arbol([[0, 1, 0, 0], [0,0, 1, 0], [0, 0, 0, 1],[1, 0, 0, 0]],4))
-#hijos de 28
-raiz.hijos[3].hijos[1].hijos[0].hijos.append(arbol([[0, 0, 1, 0], [0,1, 0, 0], [1, 0, 0, 0],[1, 0, 0, 0]],4))
-#hijos de 29
-raiz.hijos[3].hijos[1].hijos[1].hijos.append(arbol([[0, 0, 1, 0], [0,0, 0, 1], [0, 1, 0, 0],[1, 0, 0, 0]],4))
-
-tipoBusqueda = 0
-ladoBusqueda = 0
-flag = 0
-limite = -1
-
-pila = [] 
-nodo = [] 
-valorBuscado = ""
-ruta = ""
-valorB = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0],[0, 0, 0, 0]]
-
-print("Bienvenido al programa de las 4 reinas")
-tipoBusqueda = int(input("¿Qué tipo de busqueda deseas hacer?\n 1 = Profundidad\n 2 = Anchura\n 3 = Profundida limitada\n 4 = Profundidad iterativa \n"))
-ladoBusqueda = int(input("¿Por qué lado deseas realizarla?\n 1 = Izquierda\n 2 = Derecha \n"))
-
-if(tipoBusqueda == 1):
-    for i in range(4):
-        for j in range(4):
-            valorB[i][j] = int(input("Ingresa las posiciones en el tablero: "))
-    pila.append(raiz) 
-    while(pila): 
-        nodo = pila.pop() 
-        print("Ruta: \n", imprimir(nodo.valor))
-        if (nodo.valor==valorB): 
-            print("Tablero (nodo) encontrado: \n", imprimir(nodo.valor))
-            flag=1 
-            break 
+    def hijo(self):
+        x,y = self.find(self.datos,'_')
+        val_list = [[x,y-1],[x,y+1],[x-1,y],[x+1,y]]
+        children = []
+        for i in val_list:
+            child = self.BuscarResultado(self.datos,x,y,i[0],i[1])
+            if child is not None:
+                child_node = Node(child,self.nivel+1,0)
+                children.append(child_node)
+        return children
+        
+    def BuscarResultado(self,puz,x1,y1,x2,y2):
+        if x2 >= 0 and x2 < len(self.datos) and y2 >= 0 and y2 < len(self.datos):
+            temp_puzle = []
+            temp_puzle = self.matriz(puz)
+            temp = temp_puzle[x2][y2]
+            temp_puzle[x2][y2] = temp_puzle[x1][y1]
+            temp_puzle[x1][y1] = temp
+            return temp_puzle
         else:
-            if(ladoBusqueda == 1):
-                for value in reversed(nodo.hijos): 
-                    pila.append (value) 
-            elif (ladoBusqueda == 2):
-                for value in nodo.hijos: 
-                    pila.append (value)
-    if(flag==0):
-        print("Tablero (nodo) encontrado :(")
-elif(tipoBusqueda == 2):
-    for i in range(4):
-        for j in range(4):
-            valorB[i][j] = int(input("Ingresa las posiciones en el tablero: "))
-    pila.append(raiz) 
-    while(pila): 
-        nodo = pila.pop(0) 
-        print("Ruta: \n", imprimir(nodo.valor)) 
-        if (nodo.valor==valorB): 
-            print("Tablero (nodo) encontrado: \n", imprimir(nodo.valor))
-            flag = 1 
-            break 
-        else:
-            if(ladoBusqueda == 1):
-                for value in nodo.hijos: 
-                    pila.append (value)
-            elif (ladoBusqueda == 2):
-                for value in reversed(nodo.hijos): 
-                    pila.append (value)
-    if(flag==0):
-        print("Tablero (nodo) encontrado :(")
-elif(tipoBusqueda == 3):
-    for i in range(4):
-        for j in range(4):
-            valorB[i][j] = int(input("Ingresa las posiciones en el tablero: "))
-    while((limite > 4) or (limite < 0)):
-        limite = int(input("¿Hasta que profundidad? (Max 4) "))
-        if((limite > 4) or (limite < 0)):
-            print("Ingrese un limite valido")
-    pila.append(raiz) 
-    while(pila):
-        nodo = pila.pop()
-        if(nodo.nivel<=limite): 
-            print("Ruta: \n", imprimir(nodo.valor))
-            print("Nivel: ",nodo.nivel)
-            if (nodo.valor==valorB): 
-                print("Tablero (nodo) encontrado: \n", imprimir(nodo.valor))
-                flag=1 
-                break  
-            else:
-                if(ladoBusqueda == 1):
-                    for value in reversed(nodo.hijos): 
-                        pila.append (value)
-                elif (ladoBusqueda == 2):
-                    for value in nodo.hijos: 
-                        pila.append (value)
-    if(flag==0):
-        print("Tablero (nodo) encontrado :(")
-elif(tipoBusqueda == 4):
-    for i in range(4):
-        for j in range(4):
-            valorB[i][j] = int(input("Ingresa las posiciones en el tablero: "))
-    while((limite > 4) or (limite < 0)):
-        limite = int(input("¿Hasta que iteración deseas llegar? (Max 4) "))
-        if((limite > 4) or (limite < 0)):
-            print("Ingrese una iteración valida")
-    
-    for itera in range (limite):
-        print("Profundidad = ",itera)
-        if(iterada(raiz,(itera+1),ladoBusqueda,valorB)):
-            break
+            return None
+
+    def matriz(self,root):
+        temp = []
+        for i in root:
+            t = []
+            for j in i:
+                t.append(j)
+            temp.append(t)
+        return temp    
+            
+    def find(self,puz,x):
+        for i in range(0,len(self.datos)):
+            for j in range(0,len(self.datos)):
+                if puz[i][j] == x:
+                    return i,j
+
+
+class Puzzle:
+    def __init__(self,size):
+        
+        self.n = size
+        self.abrir = []
+        self.cerrar = []
+
+    def acpt(self):
+        
+        puz = []
+        for i in range(0,self.n):
+            temp = input().split(" ")
+            puz.append(temp)
+        return puz
+
+    def f(self,iniciar,goal):
+        
+        return self.h(iniciar.datos,goal)+iniciar.nivel
+
+    def h(self,iniciar,goal): 
+        
+        temp = 0
+        for i in range(0,self.n):
+            for j in range(0,self.n):
+                if iniciar[i][j] != goal[i][j] and iniciar[i][j] != '_':
+                    temp += 1
+        return temp
+        
+
+    def ingresardat(self):
+        cont=0
+        print("Instrucciones: para ingresar un espacio en blanco se debera ingresar un _")
+        print("Por ejemplo \n 1 2 3 \n 4 _ 6 \n 7 5 8")
+        print("\n Ahora escriba su matriz \n")
+        iniciar = self.acpt()
+        print("Ingrese la matriz en el estado final \n")        
+        goal = self.acpt()
+
+        iniciar = Node(iniciar,0,0)
+        iniciar.fvalores = self.f(iniciar,goal)
+
+        self.abrir.append(iniciar)
+        print("\n\n")
+        while True:
+            cont = cont + 1
+            cur = self.abrir[0]
+            print("***********")
+            print("iterado",cont)
+            print("***********\n")
+            for i in cur.datos:
+                for j in i:
+                    print(j,end=" ")
+                print("")
+            if(self.h(cur.datos,goal) == 0):
+                break
+            for i in cur.hijo():
+                i.fvalores = self.f(i,goal)
+                self.abrir.append(i)
+            self.cerrar.append(cur)
+            del self.abrir[0]
+
+            self.abrir.sort(key = lambda x:x.fvalores,reverse=False)
+
+tipoBusqueda=0
+
+tipoBusqueda = int(input("Ingrese una opcion \n1.- Solucion puzzle utilizando heuristica de piezas mal colocadas \n2.- Solucion puzzle utilizando heuristica manhattan \n"))
+if(tipoBusqueda==1):
+    print("Solucion puzzle utilizando heuristica de piezas mal colocadas")
+    puz = Puzzle(3)
+    puz.ingresardat()
+elif(tipoBusqueda==2):
+    print("Solucion puzzle utilizando heuristica manhattan")
+
+
